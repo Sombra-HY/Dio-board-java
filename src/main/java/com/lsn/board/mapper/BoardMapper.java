8ba8e7ebda69802cf0f404638f3623efad949fc9
Lucas Sombra <l.sombra.nascimento@gmail.com>
2025-08-07 21:20:10 -0300

    mapper add Tocard

--- src/main/java/com/lsn/board/mapper/BoardMapper.java
+++ src/main/java/com/lsn/board/mapper/BoardMapper.java
@@ -1,11 +1,13 @@
 package com.lsn.board.mapper;
 import com.lsn.board.dto.BoardDto;
+import com.lsn.board.dto.CardDto;
 import com.lsn.board.model.Board;
+import com.lsn.board.model.Card;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface BoardMapper {
     Board toBoard(BoardDto boardDto);
-
+    Card toCard(CardDto cardDto);
 }
 
