277d9d28cf02d76a8147ec66349aaecbe17d9e4a
Lucas Sombra <l.sombra.nascimento@gmail.com>
2025-08-07 21:20:46 -0300

    service add card

--- src/main/java/com/lsn/board/controller/TasksController.java
+++ src/main/java/com/lsn/board/controller/TasksController.java
@@ -1,7 +1,9 @@
 package com.lsn.board.controller;
 
+import com.lsn.board.dto.CardDto;
 import com.lsn.board.model.Board;
 import com.lsn.board.model.Card;
+import com.lsn.board.model.CollumBoard;
 import com.lsn.board.service.ManagerTasksService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,27 +11,28 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/boards")
+@RequestMapping("/boards/{id}")
 public class TasksController {
     private final ManagerTasksService managerTasksService;
 
-    @PostMapping("/{id}/save")
-    public ResponseEntity<Board> addCard(@RequestBody Card card, Long idCollum) {
-        managerTasksService.addCard(card,idCollum);
-        return ResponseEntity.ok().body(null);
+    @PostMapping("/save")
+    public ResponseEntity<CollumBoard> addCard(@RequestBody CardDto card, Long idCollum) {
+        return ResponseEntity.ok().body(
+                managerTasksService.addCard(card,idCollum)
+        );
     }
 
-    @DeleteMapping("/{id}/save")
+    @DeleteMapping("/save")
     public ResponseEntity<Board> removeCard(Card card, Long idCollum) {
         return ResponseEntity.ok().body(null);
     }
 
-    @PutMapping("/{id}/save")
+    @PutMapping("/save")
     public ResponseEntity<Board> updateCard(Card card, Long idCollum) {
         return ResponseEntity.ok().body(null);
     }
 
-    @PostMapping(value = "/{id}/save")
+    @PostMapping(value = "/save")
     public ResponseEntity<Board> moveToNextColumm(@PathVariable String id, @RequestBody Long idCollum, @RequestBody Long card) {
         return ResponseEntity.ok().body(null);
     }
